26e484b9d760b666a57da88fec1df50b3c07671e
Flyguy86 <BJGarcia@gmail.com>
2021-01-04 10:09:52 -0800

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 sudo chown -R ubuntu:ubuntu ~
 cd /home/ubuntu/jitsi-meet-torture
-mvn test -D max_users_tests.max_users=4 -D org.jitsi.malleus.duration=3600 -D jitsi-meet.instance.url="https://dev.myceliuminc.net" -D jitsi-meet.instance.backend-url="https://dev-api.myceliuminc.net" -D jitsi-meet.tests.toRun="MaxUsersTest"
+xvfb-run mvn test \
+-D max_users_tests.max_users=1 \
+-D org.jitsi.malleus.duration=3600 \
+-D jitsi-meet.instance.url="https://dev.myceliuminc.net" \
+-D jitsi-meet.instance.backend-url="https://dev-api.myceliuminc.net" \
+-D jitsi-meet.tests.toRun="MaxUsersTest" \
+-D jitsi-meet.instance.party-name="testparty"
