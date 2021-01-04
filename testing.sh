cf511ce138a46096b40711a154e86381eed99b78
Flyguy86 <BJGarcia@gmail.com>
2021-01-04 12:57:35 -0800

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
+if [ -z "$1" ] ; then
+    PARTYNAME=testparty
+else
+    PARTYNAME=$1
+fi
+
+if [ -z "$2" ] ; then
+    INSTANCE=https://dev.myceliuminc.net
+else
+    INSTANCE=$2
+fi
+
+if [ -z "$3" ] ; then
+    API=https://dev-api.myceliuminc.net
+else
+    API=$3
+fi
+
+if [ -z "$4" ] ; then
+    TEST=MaxUsersTest
+else
+    TEST=$4
+fi
+
+if [ -z "$5" ] ; then
+    DURATION=3600
+else
+    DURATION=$5
+fi
+
 sudo chown -R ubuntu:ubuntu ~
 cd /home/ubuntu/jitsi-meet-torture
+
 xvfb-run mvn test \
--D max_users_tests.max_users=1 \
--D org.jitsi.malleus.duration=3600 \
--D jitsi-meet.instance.url="https://dev.myceliuminc.net" \
--D jitsi-meet.instance.backend-url="https://dev-api.myceliuminc.net" \
--D jitsi-meet.tests.toRun="MaxUsersTest" \
--D jitsi-meet.instance.party-name="testparty"
+-D org.jitsi.malleus.duration=$DURATION \
+-D jitsi-meet.instance.url="$INSTANCE" \
+-D jitsi-meet.instance.backend-url="$API" \
+-D jitsi-meet.tests.toRun="$TEST" \
+-D jitsi-meet.instance.party-name="$PARTYNAME"
+
